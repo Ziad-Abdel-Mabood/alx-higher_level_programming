f0f97eff0efa473a4ede476c427ec9d506139787
ziad <ziadabdelmabood@gmail.com>
2024-01-09 04:02:45 +0200

    [Proj 3][Task 6] used str.format
    went back to older impelementation with a lot of conditions so that I can print exactly like desired format WHILE USING str.format which was what failed the checks in previous commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix)):
-        print(*matrix[row])
+    for row in matrix:
+        for x in range(len(row)):
+           print("{:d}".format(row[x]), end='')
+           if x < (len(row)-1):
+               print(" ", end='')
+        print("")
