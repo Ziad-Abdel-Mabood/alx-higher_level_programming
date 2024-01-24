112db68b8ad2ec777d1d6da0f7d38247de7857cf
ziad <ziadabdelmabood@gmail.com>
2024-01-24 12:49:03 +0200

    [Proj 0x01][Task 8] fix style

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= ord('a') and ord(c) <= ord ('z'):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
     else:
         return False
@@ -8,6 +8,6 @@ def islower(c):
 
 def uppercase(str):
     for c in str:
-        print ("{:c}"
-                .format(ord(c) if not islower(c) else ord(c) - 32), end = "")
+        print("{:c}"
+              .format(ord(c) if not islower(c) else ord(c)-32), end="")
     print("")
