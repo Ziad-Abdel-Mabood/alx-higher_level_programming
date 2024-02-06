c66c7e32f3df7212f73d2f4aedb8083b09dcd9ee
ziad <ziadabdelmabood@gmail.com>
2024-02-07 01:55:31 +0200

    task 0 trying fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""read_file function"""
+
+
+
 def read_file(filename=""):
+    """reads filename with utf-8"""
     with open(filename, 'r', encoding="utf-8") as file:
         data = file.read()
 
-    print(data)
+    print(data, end="")
