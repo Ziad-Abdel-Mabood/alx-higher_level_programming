559d285f6a337d6b114438fa3dbabcd29c6c3c8c
ziad <ziadabdelmabood@gmail.com>
2024-02-07 02:24:31 +0200

    proj 0x0B, task 1, write task file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,10 +2,8 @@
 """read_file function"""
 
 
-
 def read_file(filename=""):
     """reads filename with utf-8"""
     with open(filename, 'r', encoding="utf-8") as file:
         data = file.read()
-
     print(data, end="")
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function definition, writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """writes into file with UTF8 encoding"""
+    with open(filename, 'w', encoding="utf-8") as file:
+        file.write(text)
