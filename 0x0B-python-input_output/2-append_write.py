960feff60c97c00176f4418960ac1b784502b904
ziad <ziadabdelmabood@gmail.com>
2024-02-07 02:42:20 +0200

    proj 0x0B, task 2, fix func name

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """function definition, appends a string to a text file"""
 
 
-def write_file(filename="", text=""):
+def append_file(filename="", text=""):
     """appends into file with UTF8 encoding"""
     with open(filename, 'a', encoding="utf-8") as file:
         return file.write(text)
