44ed3ff073bf2d4f91a77699ab02bd6c94f9861e
ziad <ziadabdelmabood@gmail.com>
2024-02-13 03:37:34 +0200

    proj 0x0C, task 1, write task file

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,1 @@
+0x0C Python almost a circle project, revising unittesting and json, and learning about args and kwargs
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+""" Base Class Module """
+
+class Base:
+    """
+    Base class
+        nb_objects: manage id attribute
+    """
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        constructor method
+
+            id : id number attr of the object
+        """
+        
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
