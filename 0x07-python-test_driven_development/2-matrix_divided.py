cec54c0f9d0bfd9eabe3c643f4f77958585a6409
Ziad-Abdel-Mabood <ziadabdlemabood@gmail.com>
2024-02-26 04:35:46 +0200

    [0x07-python-test_driven_development], task 1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" matrix_divided function module """
+
+
+def matrix_divided(matrix, div):
+    """ Divides all elements of matrix by div
+    Args:
+        matrix: list of lists containing int or float
+        div: number to divide matrix by
+    Return:
+        list: List of lists which is diveded matrix
+    Raises:
+        TypeError: if matrix is not list of lists containing int or float
+        TypeError: if sublists are not all same size.
+        Type Error: If div is not int or gloat.
+        ZeroDivisionError: if div is zero.
+    """
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix myst be a matrix (list of lists) " +
+                        "of integer/floats")
+    for row in matrix:
+        if not isinstance(row, list) or len(row) == 0:
+            raise TypeError("matrix myst be a matrix (list of lists) " +
+                            "of integer/floats")
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for x in row:
+            if not isinstance(x, (int, float)):
+                raise TypeError("matrix myst be a matrix (list of lists) " +
+                                "of integer/floats")
+    return [[round(x/div, 2) for x in row] for row in matrix]
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
