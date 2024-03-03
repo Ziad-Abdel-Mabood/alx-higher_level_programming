bcb120868fcb533ba2e19613efb9a84c76758a5e
ziad <ziadabdelmabood@gmail.com>
2024-03-03 14:47:02 +0200

    proj python 0x07, task 1, adding testcases

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -19,17 +19,17 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a matrix (list of lists) " +
-                        "of integer/floats")
+                        "of integers/floats")
     for row in matrix:
         if not isinstance(row, list) or len(row) == 0:
             raise TypeError("matrix must be a matrix (list of lists) " +
-                            "of integer/floats")
+                            "of integers/floats")
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         for x in row:
             if not isinstance(x, (int, float)):
                 raise TypeError("matrix must be a matrix (list of lists) " +
-                                "of integer/floats")
+                                "of integers/floats")
     return [[round(x/div, 2) for x in row] for row in matrix]
 
 
