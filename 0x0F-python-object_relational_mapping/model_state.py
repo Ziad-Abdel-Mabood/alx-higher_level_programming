90d59e3bda584476f574dbcd53a4236f19c44e19
ziad <ziadabdelmabood@gmail.com>
2024-04-17 15:03:23 +0200

    latest work

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""contains the class definition of a State and an instance Base = declarative_base():"""
+import sqlalchemy
+import MySQLdb
+
+class State:
+	""" class definition of a state """
+	pass
