b919aedf9559fd19f6f1dd0bf110813e33e7ab0c
ziad <ziadabdelmabood@gmail.com>
2024-04-18 00:05:55 +0200

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,42 @@
 #!/usr/bin/python3
-"""contains the class definition of a State and an instance Base = declarative_base():"""
-import sqlalchemy
+"""
+contains the class definition of a State
+and an instance Base = declarative_base()
+"""
 import MySQLdb
+import sys
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
-class State:
-	""" class definition of a state """
-	pass
+Base = declarative_base()
+username = sys.argv[1]
+password = sys.argv[2]
+dbname = sys.argv[3]
+
+db = MySQLdb.connect(host='localhost',
+                     user=username,
+                     passwd=password,
+                     db=dbname,
+                     port=3306)
+
+
+class State(Base):
+    """
+    State class:
+
+    - inherits from Base Tips.
+    - links to the MySQL table states.
+    - class attribute id that represents a column of an auto-generated,
+      unique integer, can't be null and is a primary key.
+    - class attribute name that represents a column of a string
+      with maximum 128 characters and can't be null.
+    """
+
+    __tablename__ = 'states'
+
+    id = Column(Integer,
+                autoincrement=True,
+                nullable=False,
+                primary_key=True,
+                unique=True)
+    name = Column(String(128), nullable=False)
